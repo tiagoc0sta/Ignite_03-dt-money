596808dd6e4cdd26c52b769342fe106c934bc2cc
Tiago Costa <tiagodpv@gmail.com>
2022-10-24 18:09:36 -0400

    add table

---
+++ 03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,9 @@
+import { SearchFormContainer } from "./styles";
+
+export function SearchForm() {
+  return(
+    <div>
+    <SearchFormContainer/>
+    </div>
+  )
+}
--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,15 +1,48 @@
 import { Header } from "../../compponents/Header";
 import { Summary } from "../../compponents/Summary";
+import { SearchFormContainer } from "./components/SearchForm/styles";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions () {
   return (
     <div>
       <Header/>
       <Summary/>
+      <SearchFormContainer/>
 
-      <table>
+
+     <TransactionsContainer>
+      <TransactionsTable>
+        <table>
+            <tbody>
+              <tr>
+                <td width="50%">Desenvolvimento de site</td>
+               
+                <td>
+                  <PriceHighLight variant="income">
+                    R$ 12.000,00
+                  </PriceHighLight>
+                  </td>                
+                <td>Venda</td>
+                <td>13/04/2022</td>
+              </tr>
+
+              <tr>
+                <td width="50%">Hamburger</td>
+                <td>
+                  <PriceHighLight variant="outcome">
+                    - R$ 59,00
+                  </PriceHighLight>
+                </td>
+                <td>Alimentação</td>
+                <td>10/04/2022</td>
+              </tr>          
+            </tbody>
+          </table>
+      </TransactionsTable>       
+     </TransactionsContainer>
         
-      </table>
+      
     </div>
   )
 }
